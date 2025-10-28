d92c780fbb567d8ca8a59a9e2d7df3f4d826b4ce
Sergio <sdzuniga64@ucatolica.edu.co>
2025-10-28 17:59:51 -0500

    FUCK v4

--- app/crud.py
+++ app/crud.py
@@ -7,4 +7,9 @@ from . import models, schemas
 async def crear_categoria(db: AsyncSession, data: schemas.CategoriaCreate):
     existe = await db.scalar(select(models.Categoria).where(models.Categoria.nombre == data.nombre))
     if existe:
-        raise HTTPException(status_co
+        raise HTTPException(status_code=409, detail="La categoría ya existe")
+    nueva = models.Categoria(**data.model_dump())
+    db.add(nueva)
+    await db.commit()
+    await db.refresh(nueva)
+    return nueva
