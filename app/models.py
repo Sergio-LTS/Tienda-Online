23b0e4cb78e7ad8d91b6831a1aa44e587125bb25
Sergio <sdzuniga64@ucatolica.edu.co>
2025-10-28 10:42:39 -0500

    modelo categoria v2

--- app/models.py
+++ app/models.py
@@ -6,3 +6,7 @@ class Categoria(Base):
     __tablename__ = "categorias"
 
     id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String(100), unique=True, nullable=False)
+    descripcion = Column(String(255))
+    activa = Column(Boolean, default=True)
+
