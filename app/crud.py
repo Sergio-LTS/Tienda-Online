38d09df84e55f188725d407aaa5dd6d14a9f801f
Sergio <sdzuniga64@ucatolica.edu.co>
2025-10-28 17:59:28 -0500

    FUCK v3

--- app/crud.py
+++ app/crud.py
@@ -2,3 +2,9 @@ from fastapi import HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from . import models, schemas
+
+#Categorias
+async def crear_categoria(db: AsyncSession, data: schemas.CategoriaCreate):
+    existe = await db.scalar(select(models.Categoria).where(models.Categoria.nombre == data.nombre))
+    if existe:
+        raise HTTPException(status_co
